b55b908b3de69907179b09bab03cbef7838ca8c3
Yordani Bonilla <b.yordani93@gmail.com>
2019-08-02 00:10:21 -0500

    Match variable declarations

--- Exercises.js
+++ Exercises.js
@@ -1,19 +1,7 @@
-          /*es par o impar*/ 
-          //input: string
-          //output: string
-          //constraints: 0
-          //edgeCases: 0
-
-          let parOImpar = num => {
-            //check if num is greater than 0 and has no remainder with modulo
-            //if true log "par" else log "impar"
-            return num > 0 && num % 2 === 0 ? "par" : "impar";
-          };
-
           let pesoEnOtroPlaneta = (planeta, peso) => {
           //accepts planet and weight arguments
           switch (planeta.toLowerCase()) {
-          //if argument is marte
+          //if argument is marte / mars
           case ("mars"):
           case ("marte"):        
           //divide weight by earth gravitity * mar's gravity
@@ -28,34 +16,34 @@
           break;
           case ("saturno"):
           case ("saturn"):
-          //divide weight by earth gravity * jupiter's gravity
+       
           let weightOnSaturn = (peso / 9.81) * 10.44;
-          document.getElementById("respuesta").innerHTML = weightOnJupiter;
+          document.getElementById("respuesta").innerHTML = weightOnSaturn;
           break;
       
           case ("urano"):
           case ("uranus"): 
-          //divide weight by earth gravity * jupiter's gravity
+
           let weightOnUranus = (peso / 9.81) * 8.87;
-          document.getElementById("respuesta").innerHTML = weightOnJupiter;
+          document.getElementById("respuesta").innerHTML = weightOnUranus;
           break;
           case ("neptuno"):
           case ("neptune"): 
-          //divide weight by earth gravity * jupiter's gravity
-          let weightOnNeptuen = (peso / 9.81) * 11.15;
-          document.getElementById("respuesta").innerHTML = weightOnJupiter;
+
+          let weightOnNeptune = (peso / 9.81) * 11.15;
+          document.getElementById("respuesta").innerHTML = weightOnNeptune;
           break;
           case ("mercurio"):
           case ("mercury"): 
-          //divide weight by earth gravity * jupiter's gravity
+
           let weightOnMercury = (peso / 9.81) * 3.7;
-          document.getElementById("respuesta").innerHTML = weightOnJupiter;
+          document.getElementById("respuesta").innerHTML = weightOnMercury;
           break;
 
           case "venus" : 
-          //divide weight by earth gravity * jupiter's gravity
+
           let weightOnVenus = (peso / 9.81) * 8.87;
-          document.getElementById("respuesta").innerHTML = weightOnJupiter;
+          document.getElementById("respuesta").innerHTML = weightOnVenus ;
           break;
             }
           };
